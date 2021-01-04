e326ded65afcee977b54b5305397ed6511b2a4cc
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 09:39:57 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,15 +2,15 @@ let form = document.querySelector('form');
 let input = document.getElementById('comment-input');
 let commentArea = document.getElementById('list');
 let counterId = document.getElementById('counter');
-let counter = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   
-  // function increment() {
-  //   counterId.innerHTML = counter;
-  //   counter++
-  // }
-  // setInterval(increment, 1000);
+  let counter = 0;
+  function increment() {
+    counterId.innerHTML = counter;
+    counter++
+  }
+  setInterval(increment, 1000);
   
   let form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
@@ -28,4 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-document.addEventListener('')
+
+
+document.addEventListener('click', () =>{
+  
+});
