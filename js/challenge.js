c75da7c8e8a71fb6af8e2c6141ce66a13f8dd6f2
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 09:30:28 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,13 +2,12 @@ let form = document.querySelector('form');
 let input = document.getElementById('comment-input');
 let commentArea = documet.getElementById('list');
 let counter = document.getElementById('counter');
+let counter = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   
-  let counter = 0;
-  
+  let form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
-    let form = document.querySelector('form');
     let input = document.getElementById('comment-input');
     let commentArea = document.getElementById('list');
     let comment = document.createElement('p');
